014cef2099eebb5c05f54963906b3530ff991735
immars <immars@gmail.com>
2015-01-12 12:03:34 +0800

    spider debug

--- taobao/taobao/settings.py
+++ taobao/taobao/settings.py
@@ -10,8 +10,22 @@
 
 BOT_NAME = 'taobao'
 
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+
+DEFAULT_REQUEST_HEADERS = {
+		'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		# Accept-Encoding:gzip, deflate, sdch
+		'Accept-Language' : 'zh-CN,zh;q=0.8',
+		'Cache-Control' : 'max-age=0',
+}
+
 ITEM_PIPELINES = {'scrapy.contrib.pipeline.images.ImagesPipeline': 1}
 IMAGES_STORE = '/home/immars/work/search/scrapy/taobao/image_data'
+
+COOKIES_ENABLED = True
+
+COOKIES_DEBUG = True
+
 
 SPIDER_MODULES = ['taobao.spiders']
 NEWSPIDER_MODULE = 'taobao.spiders'
